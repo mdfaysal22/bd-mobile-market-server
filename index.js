1c629c74a2e78a4d797d3b9078787af850da0c60
Md Faysal <mdfaysal.freelancer@gmail.com>
2022-11-26 00:11:35 +0600

    Create Products API

--- index.js
+++ index.js
@@ -23,22 +23,40 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         const usersCollection = client.db("BD-Mobile-Market").collection("users");
+        const productsCollection = client.db("BD-Mobile-Market").collection("products");
 
-        app.post('/users', async(req, res) => {
+        app.post('/users', async (req, res) => {
             const user = req.body;
-            const result = await usersCollection.insertOne(user);
-            res.send(result);
+            const query = { email: user.email }
+            const existingUser = await usersCollection.find(query).toArray()
+            if (existingUser.length === 0) {
+                const result = await usersCollection.insertOne(user);
+                res.send(result);
+            }
+
         })
-        app.get('/users', async(req, res) => {
+        app.get('/users', async (req, res) => {
             const query = {};
             const users = await usersCollection.find(query).toArray();
             res.send(users);
         })
+
+
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.send(result);
+        })
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+        })
     }
-    finally{}
+    finally { }
 }
 
 run().catch(err => console.log(err))
