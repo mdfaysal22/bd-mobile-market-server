adb5a93731adc5f3a1f6351eafa1dffb70701ff4
Md Faysal <mdfaysal.freelancer@gmail.com>
2022-11-27 10:52:23 +0600

    Resolved Delete System

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ async function run() {
 
         app.delete('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: id };
+            const query = { _id: ObjectId(id) };
             const result = await productsCollection.deleteOne(query);
             res.send(result);
         })
